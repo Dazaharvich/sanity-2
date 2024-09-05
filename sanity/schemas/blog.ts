a5283a6d7d084b259c1133635b7e111fef864098
Dazaharvich <d.azaharvichp@gmail.com>
2024-09-04 20:04:33 -0400

    first commit

---
+++ sanity/schemas/blog.ts
@@ -0,0 +1,42 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+export default{
+    name: 'blog',
+    type: 'document',
+    title: 'Base de Conocimientos',
+    fields: [
+        {
+            name: 'title',
+            type: 'string',
+            title: 'Titulo del Caso',
+        },
+        {
+            name: 'slug',
+            type: 'slug',
+            title: 'URL del Caso',
+            options: {
+                source: 'title',
+            },
+        },
+        {
+            name: 'titleImage',
+            type: 'image',
+            title: 'Titulo Imagen',
+        },
+        {
+            name: 'smallDescription',
+            type: 'text',
+            title: 'Descripción',
+        },
+        {
+            name: 'content',
+            type: 'array',
+            title: 'Solución',
+            of: [
+                {
+                    type: 'block',
+                }
+            ]
+        }
+    ]
+}
